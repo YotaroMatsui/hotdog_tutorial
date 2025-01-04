fe75f1af477e325c4c1e459e233623113ff2cab2
YotaroMatsui <b10matsuwo.10.madscience@gmail.com>
2025-01-05 01:49:39 +0900

    move view component

--- src/components/view.rs
+++ src/components/view.rs
@@ -1,1 +1,39 @@
+use dioxus::prelude::*;
 
+use crate::backend::save_dog;
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
+#[component]
+pub fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    rsx! {
+        div { id: "dogView",
+            img { src: img_src.cloned().unwrap_or_default(), alt: "dog" }
+        }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button {
+                id: "save",
+                onclick: move |_| async move {
+                    let current_image = img_src.cloned().unwrap();
+                    img_src.restart();
+                    let _ = save_dog(current_image).await;
+                },
+                "save!"
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use backend::save_dog;
 use dioxus::prelude::*;
 
 mod components;
@@ -27,42 +26,5 @@ fn App() -> Element {
     rsx! {
         document::Stylesheet { href: CSS }
         Router::<Route>{}
-    }
-}
-
-
-#[derive(serde::Deserialize)]
-struct DogApi {
-    message: String,
-}
-
-#[component]
-fn DogView() -> Element {
-    let mut img_src = use_resource(|| async move {
-        reqwest::get("https://dog.ceo/api/breeds/image/random")
-            .await
-            .unwrap()
-            .json::<DogApi>()
-            .await
-            .unwrap()
-            .message
-    });
-
-    rsx! {
-        div { id: "dogView",
-            img { src: img_src.cloned().unwrap_or_default(), alt: "dog" }
-        }
-        div { id: "buttons",
-            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
-            button {
-                id: "save",
-                onclick: move |_| async move {
-                    let current_image = img_src.cloned().unwrap();
-                    img_src.restart();
-                    let _ = save_dog(current_image).await;
-                },
-                "save!"
-            }
-        }
     }
 }
